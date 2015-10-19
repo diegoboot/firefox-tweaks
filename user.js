55e667c43bb62dafb7f0fb844ee47515e3ccc9c0
dfkt <github@dfkt.tk>
2015-10-19 04:40:25 +0200

    Update user.js

--- user.js
+++ user.js
@@ -53,7 +53,6 @@ user_pref('media.navigator.enabled', false);
 user_pref('media.getusermedia.screensharing.enabled', false);
 user_pref('media.getusermedia.screensharing.allowed_domains', '');
 
-
 // Disable DNS proxy bypass:
   // https://superuser.com/questions/103593/how-to-do-dns-through-a-proxy-in-firefox
   // https://bugzilla.mozilla.org/show_bug.cgi?id=134105
@@ -123,7 +122,7 @@ user_pref('datareporting.healthreport.uploadEnabled', false);
 user_pref('datareporting.policy.dataSubmissionEnabled', false);
 user_pref('datareporting.policy.dataSubmissionEnabled.v2', false);
 
-// Disable "Heartbeat"
+// Disable "Heartbeat":
   // https://wiki.mozilla.org/Advocacy/heartbeat
 user_pref('browser.selfsupport.url', '');
 
@@ -154,33 +153,9 @@ user_pref('browser.safebrowsing.reportPhishURL', '');
 // user_pref('browser.safebrowsing.updateURL', '');
 // user_pref('urlclassifier.keyupdatetime.https://sb-ssl.google.com/safebrowsing/newkey', '');
 
-// Remove feed content handlers:
-  // http://kb.mozillazine.org/Browser.contentHandlers.types.*.uri
-  // Yahoo RSS handler:
-user_pref('browser.contentHandlers.types.0.title', '');
-user_pref('browser.contentHandlers.types.0.type', '');
-user_pref('browser.contentHandlers.types.0.uri', '');
-
-// Remove website protocol handlers:
-  // http://kb.mozillazine.org/Gecko.handlerService.schemes.%28protocol%29.*.uriTemplate
-  // Mibbit:
-user_pref('gecko.handlerService.schemes.irc.0.name', '');
-user_pref('gecko.handlerService.schemes.irc.0.uriTemplate', '');
-user_pref('gecko.handlerService.schemes.ircs.0.name', '');
-user_pref('gecko.handlerService.schemes.ircs.0.uriTemplate', '');
-  // Yahoo Mail:
-user_pref('gecko.handlerService.schemes.mailto.0.name', '');
-user_pref('gecko.handlerService.schemes.mailto.0.uriTemplate', '');
-  // Gmail:
-user_pref('gecko.handlerService.schemes.mailto.1.name', '');
-user_pref('gecko.handlerService.schemes.mailto.1.uriTemplate', '');
-  // 30 Boxes:
-user_pref('gecko.handlerService.schemes.webcal.0.name', '');
-user_pref('gecko.handlerService.schemes.webcal.0.uriTemplate', '');
-
 // Disable WebGL:
-  // https://security.stackexchange.com/questions/13799/is-webgl-a-security-concern
   // http://www.contextis.com/resources/blog/webgl-new-dimension-browser-exploitation/
+  // https://security.stackexchange.com/questions/13799/is-webgl-a-security-concern
 user_pref('webgl.disabled', true);
 user_pref('webgl.disable-extensions', true);
 
@@ -195,7 +170,7 @@ user_pref('javascript.options.asmjs', false);
   // https://github.com/iSECPartners/publications/blob/master/presentations/SVG_Security-rdegraaf-bh_us_2014.pdf
 user_pref('gfx.font_rendering.opentype_svg.enabled', false);
 
-// Install unsigned addons in Aurora/Dev-Edition/etc. (ironically, this was needed for security-enhancing addons like Privacy Badger, HTTPS Everywhere, etc. - don't be stupid and install just any random unsigned addon):
+// Install unsigned addons in Aurora/Dev-Edition/etc. (Ironically, this was needed for security-enhancing addons like Privacy Badger, HTTPS Everywhere, etc. - don't be stupid and install just any random unsigned addon):
 user_pref('xpinstall.signatures.required', false);
 
 // Speed up security delay when installing add-ons:
@@ -230,9 +205,9 @@ user_pref('full-screen-api.warning.timeout', 0);
 // Disable fullscreen animation:
 user_pref('browser.fullscreen.animate', false);
 
-// Get rid of "Do you really want to leave this site?" popups:
-  // https://support.mozilla.org/en-US/questions/1043508
-user_pref('dom.disable_beforeunload', true);
+// Disable tab animation:
+  // http://www.askvg.com/how-to-disable-animation-while-opening-new-tab-in-mozilla-firefox-4-0/
+user_pref('browser.tabs.animate', false);
 
 // Don't warn when closing tabs:
 user_pref('browser.tabs.warnOnClose', false);
@@ -241,17 +216,20 @@ user_pref('browser.tabs.warnOnCloseOtherTabs', false);
 // Don't warn when opening about:config:
 user_pref('general.warnOnAboutConfig', false);
 
+// Get rid of "Do you really want to leave this site?" popups:
+  // https://support.mozilla.org/en-US/questions/1043508
+user_pref('dom.disable_beforeunload', true);
+
 // Prevent sites from messing with the right-click menu:
 user_pref('dom.event.contextmenu.enabled', false);
 
 // Prevent sites/popups from messing with certain UI elements:
   // http://kb.mozillazine.org/Prevent_websites_from_disabling_new_window_features
-  // Always show the URL bar:
-user_pref('dom.disable_window_open_feature.location', true);
-  // Allow to resize the window:
-user_pref('dom.disable_window_open_feature.resizable', true);
+user_pref('dom.disable_window_open_feature.location', true); // Always show the URL bar
+user_pref('dom.disable_window_open_feature.resizable', true); // Allow to resize the window
 
-// De-crap new tab page, get rid of ads:
+// De-crap new tab page, get rid of "directory tiles" ads:
+  // http://thenextweb.com/apps/2014/08/28/mozilla-rolls-sponsored-tiles-firefox-nightlys-new-tab-page/
 user_pref('browser.newtab.preload', false);
 user_pref('browser.newtab.url', 'about:blank');
 user_pref('browser.newtabpage.directory.ping', '');
@@ -260,14 +238,10 @@ user_pref('browser.newtabpage.enabled', false);
 user_pref('browser.newtabpage.enhanced', false);
 user_pref('browser.newtabpage.introShown', true);
 
-// Disable tab animation:
-  // http://www.askvg.com/how-to-disable-animation-while-opening-new-tab-in-mozilla-firefox-4-0/
-user_pref('browser.tabs.animate', false);
-
 // Disable broken auto-scrolling via middle-click:
 user_pref('general.autoScroll', false);
 
-// Prettier default fonts (for Windows, at least - requires ttf-mscorefonts on *nix):
+// Prettier default fonts (for Windows, at least - might require ttf-mscorefonts on *nix):
 user_pref('font.name.monospace.x-unicode', 'Lucida Console');
 user_pref('font.name.monospace.x-western', 'Lucida Console');
 user_pref('font.name.sans-serif.x-unicode', 'Segoe UI');
@@ -299,17 +273,6 @@ user_pref('media.eme.apiVisible', false);
 user_pref('media.eme.enabled', false);
 user_pref('browser.eme.ui.enabled', false);
 
-// Disable "Reader Mode":
-user_pref('reader.parse-on-load.enabled', false);
-// user_pref('readinglist.server', '');
-
-// Disable "Pocket":
-  // http://venturebeat.com/2015/06/09/mozilla-responds-to-firefox-user-backlash-over-pocket-integration/
-user_pref('browser.pocket.api', '');
-user_pref('browser.pocket.enabled', false);
-user_pref('browser.pocket.oAuthConsumerKey', '');
-user_pref('browser.pocket.site', '');
-
 // Disable "Firefox Hello" WebRTC malware:
   // https://www.mozilla.org/en-US/privacy/firefox-hello/
 user_pref('loop.CSP', '');
@@ -318,7 +281,14 @@ user_pref('loop.feedback.baseUrl', '');
 user_pref('loop.oauth.google.scope', '');
 user_pref('loop.server', '');
 
-// Disable "Social" crap:
+// Disable "Pocket":
+  // http://venturebeat.com/2015/06/09/mozilla-responds-to-firefox-user-backlash-over-pocket-integration/
+user_pref('browser.pocket.api', '');
+user_pref('browser.pocket.enabled', false);
+user_pref('browser.pocket.oAuthConsumerKey', '');
+user_pref('browser.pocket.site', '');
+
+// Disable "social" crap:
   // http://www.ghacks.net/2013/04/10/mozilla-adds-cliqz-msnnow-and-mixi-as-social-providers-to-firefox/
 user_pref('social.directories', '');
 user_pref('social.remote-install.enabled', false);
@@ -327,14 +297,15 @@ user_pref('social.shareDirectory', '');
 user_pref('social.toast-notifications.enabled', false);
 user_pref('social.whitelist', '');
 
+// Disable "Reader Mode":
+user_pref('reader.parse-on-load.enabled', false);
+// user_pref('readinglist.server', '');
+
 // Disable integrated PDF reader:
+  // https://blog.mozilla.org/security/2015/08/06/firefox-exploit-found-in-the-wild/
   // If you're going to use an external PDF reader, *don't* use Adobe PDF bloatware. Use a sane reader, such as SumatraPDF.
 user_pref('pdfjs.disabled', true);
 user_pref('pdfjs.enableWebGL', false);
-
-// Disable "Snippets" (Mozilla content shown on about:home):
-  // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_mozilla-content
-user_pref('browser.aboutHomeSnippets.updateUrl', '');
 
 // Disable various useless and/or intrusive web APIs:
   // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections
@@ -354,3 +325,31 @@ user_pref('dom.vr.oculus050.enabled', false);
 user_pref('dom.webnotifications.enabled', false); // https://developer.mozilla.org/en-US/docs/Web/API/notification
 user_pref('dom.webnotifications.serviceworker.enabled', false); // https://developer.mozilla.org/en-US/docs/Web/API/notification
 user_pref('media.webspeech.recognition.enable', false); // https://wiki.mozilla.org/HTML5_Speech_API
+
+// Remove feed content handlers:
+  // http://kb.mozillazine.org/Browser.contentHandlers.types.*.uri
+  // Yahoo RSS handler:
+user_pref('browser.contentHandlers.types.0.title', '');
+user_pref('browser.contentHandlers.types.0.type', '');
+user_pref('browser.contentHandlers.types.0.uri', '');
+
+// Remove website protocol handlers:
+  // http://kb.mozillazine.org/Gecko.handlerService.schemes.%28protocol%29.*.uriTemplate
+  // Mibbit:
+user_pref('gecko.handlerService.schemes.irc.0.name', '');
+user_pref('gecko.handlerService.schemes.irc.0.uriTemplate', '');
+user_pref('gecko.handlerService.schemes.ircs.0.name', '');
+user_pref('gecko.handlerService.schemes.ircs.0.uriTemplate', '');
+  // Yahoo Mail:
+user_pref('gecko.handlerService.schemes.mailto.0.name', '');
+user_pref('gecko.handlerService.schemes.mailto.0.uriTemplate', '');
+  // Gmail:
+user_pref('gecko.handlerService.schemes.mailto.1.name', '');
+user_pref('gecko.handlerService.schemes.mailto.1.uriTemplate', '');
+  // 30 Boxes:
+user_pref('gecko.handlerService.schemes.webcal.0.name', '');
+user_pref('gecko.handlerService.schemes.webcal.0.uriTemplate', '');
+
+// Disable "Snippets" (Mozilla content shown on about:home):
+  // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_mozilla-content
+user_pref('browser.aboutHomeSnippets.updateUrl', '');
